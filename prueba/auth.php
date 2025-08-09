c2dbf3d45038944a14917aea947a18cab83f7749
Daniel Obando <obandovasquezdanielisaac@gmail.com>
2025-08-08 18:18:19 -0600

    Modificacion de los archivos en mi propia rama

--- prueba/auth.php
+++ prueba/auth.php
@@ -4,7 +4,10 @@ if ($_SERVER["REQUEST_METHOD"] === "POST") {
     $email = htmlspecialchars($_POST["email"]);
 
     echo "Gracias, $nombre. Hemos recibido tu mensaje al correo: $email.";
+    echo "Fecha de recepción: " . date("d/m/Y H:i:s");
+    echo "Modificado por Daniel";
 } else {
     echo "Método no permitido.";
+    echo "Modificado por Daniel";
 }
 ?>
